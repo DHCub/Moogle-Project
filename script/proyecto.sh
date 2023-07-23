4f477553917e9c271ce8e7d0f2741a5154159140
DHCub <127904329+DHCub@users.noreply.github.com>
2023-07-22 23:22:34 -0400

    proyecto.sh can now be executed from any console working directory
    still needs to be executed from the console

--- script/proyecto.sh
+++ script/proyecto.sh
@@ -1,26 +1,32 @@
+# this line is added so the script can be run from any folder in the system, not only the location of the script in the project
+ABS="$(dirname "$(readlink -f -- "$0")")"
+# "--" added so file names like -name/ won't break the command, readlink to resolve symlinks
+
+cd "$ABS"
+
 if [ $# -lt 1 ]; then
 	echo "Script takes one of the following arguments:"
 	echo "* 'run': Runs the project"
-	echo "* 'report': Generates Reporte.pdf in ../informe"
-	echo "* 'slides': Generates Presentación.pdf in ../presentación"
-	echo "* 'show_report <pdf viewer>': Opens ../informe/Reporte.pdf (generates it first if non-existent) with specified pdf viewer or default one if it wasn't specified" 
-	echo "* 'show_slides <pdf viewer>': Opens ../presentación/Presentación.pdf (generates it first if non-existent) with specified pdf viewer or default one if it wasn't specified" 
-	echo "* 'clean': Cleans unnecessary files generated upon latex compilation in ../informe and ../presentación"
+	echo "* 'report': Generates Report pdf"
+	echo "* 'slides': Generates Presentation pdf"
+	echo "* 'show_report <pdf viewer>': Opens Report pdf (generates it first if non-existent) with specified pdf viewer or default one if it wasn't specified" 
+	echo "* 'show_slides <pdf viewer>': Opens Presentation pdf (generates it first if non-existent) with specified pdf viewer or default one if it wasn't specified" 
+	echo "* 'clean': Cleans unnecessary files generated upon latex compilation of Report and Presentation pdfs"
 
-elif [ $1 == "run" ]; then
+elif [ "$1" = "run" ]; then
 	cd ..
 	dotnet watch run --project MoogleServer
 
-elif [ $1 == "report" ]; then
+elif [ "$1" = "report" ]; then
 	cd ../informe
 	pdflatex Reporte.tex
 
-elif [ $1 == "slides" ]; then
+elif [ "$1" = "slides" ]; then
 	cd ../presentación
 	pdflatex Presentación.tex
 	pdflatex Presentación.tex
 
-elif [ $1 == "show_slides" ]; then
+elif [ "$1" = "show_slides" ]; then
 	cd ../presentación
 
 	if ! test -f "Presentación.pdf"; then
@@ -29,10 +35,10 @@ elif [ $1 == "show_slides" ]; then
 		cd ../presentación; fi
 
 	if [ $# -gt 1 ]; then
-		$2 Presentación.pdf
+		"$2" Presentación.pdf
 	else open Presentación.pdf; fi
 
-elif [ $1 == "show_report" ]; then
+elif [ "$1" = "show_report" ]; then
 	cd ../informe
 
 	if ! test -f "Reporte.pdf";then
@@ -41,10 +47,10 @@ elif [ $1 == "show_report" ]; then
 		cd ../informe; fi
 
 	if [ $# -gt 1 ]; then
-		$2 Reporte.pdf
+		"$2" Reporte.pdf
 	else open Reporte.pdf; fi
 
-elif [ $1 == "clean" ]; then
+elif [ "$1" = "clean" ]; then
 	cd ../presentación
 	if test -f "Presentación.aux"; then rm "Presentación.aux";fi
 	if test -f "Presentación.log"; then rm "Presentación.log";fi
